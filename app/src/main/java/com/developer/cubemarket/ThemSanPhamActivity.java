30e7120c65c076add472b0b3efeb42f073273c18
chaunhan <justinngo159753@gmail.com>
2022-11-10 01:03:50 +0700

    Thêm Sản Phẩm

---
+++ app/src/main/java/com/developer/cubemarket/ThemSanPhamActivity.java
@@ -0,0 +1,14 @@
+package com.developer.cubemarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThemSanPhamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_them_san_pham);
+    }
+}

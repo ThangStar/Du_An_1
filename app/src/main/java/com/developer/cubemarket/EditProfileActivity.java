15bfe10ef140150957311afb2b0a3df610ad791b
chaunhan <justinngo159753@gmail.com>
2022-11-10 19:50:12 +0700

    Update Profile

--- app/src/main/java/com/developer/cubemarket/EditProfileActivity.java
+++ app/src/main/java/com/developer/cubemarket/EditProfileActivity.java
@@ -7,17 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 import com.developer.cubemarket.MODEL.DAO.DaoUser;
 import com.developer.cubemarket.MODEL.OOP.User;
 import com.google.android.material.textfield.TextInputEditText;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EditProfileActivity extends AppCompatActivity {
 
-    TextInputEditText Email,Ten;
-    TextView tv_Email,tv_Ten;
+    TextInputEditText Email,Ten,Sdt,ChucVu,Password;
+    TextView tv_Email,tv_Ten,tv_Sdt,tv_ChucVu,tv_Pass;
     Button btn_capnhat;
     DaoUser userDao;
     List<User> list;
@@ -29,12 +32,24 @@ public class EditProfileActivity extends AppCompatActivity {
 // Ánh Xạ
         Email = findViewById(R.id.edt_a);
         Ten = findViewById(R.id.edt_b);
+        Password = findViewById(R.id.edt_c);
+        Sdt = findViewById(R.id.edt_d);
+        ChucVu = findViewById(R.id.edt_e);
 
         tv_Email = findViewById(R.id.tv_email);
         tv_Ten = findViewById(R.id.tv_name);
+        tv_Pass = findViewById(R.id.tv_pass);
+        tv_Sdt = findViewById(R.id.tv_sdt);
+        tv_ChucVu = findViewById(R.id.tv_chucvu);
 
         String email = Email.getText().toString();
         String ten = Ten.getText().toString();
+        String pass = Password.getText().toString();
+        String sdt = Sdt.getText().toString();
+        String chucVu = ChucVu.getText().toString();
+
+        userDao = new DaoUser(EditProfileActivity.this);
+
 
         btn_capnhat= findViewById(R.id.btn_capnhat);
 // bắt sự kiện ấn nút cập nhật
@@ -46,8 +61,12 @@ public class EditProfileActivity extends AppCompatActivity {
                     FancyToast.makeText(EditProfileActivity.this,"Hãy kiểm tra lại thông tin", FancyToast.LENGTH_SHORT,FancyToast.ERROR,false).show();
                 }else {
                     User user = new User();
+                    user.setId(1);
                     user.setTen(ten);
                     user.setGmail(email);
+                    user.setPassword(pass);
+                    user.setPhone(sdt);
+                    user.setChucvu(1);
                     userDao.updete_user(user);
                     FancyToast.makeText(EditProfileActivity.this,"Đã cập nhật", FancyToast.LENGTH_SHORT,FancyToast.SUCCESS,false).show();
                 }
@@ -59,8 +78,12 @@ public class EditProfileActivity extends AppCompatActivity {
     public boolean validate(){
         String email = Email.getText().toString();
         String ten = Ten.getText().toString();
-        int a ,b ;
-
+        String pass = Password.getText().toString();
+        String sdt = Sdt.getText().toString();
+        String chucVu = ChucVu.getText().toString();
+        int a ,b, c, d ;
+        Pattern ptrn = Pattern.compile("(0/91)?[7-9][0-9]{9}");
+        Matcher match = ptrn.matcher(sdt);
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
         if (!email.matches(emailPattern)){
             tv_Email.setText("Email không hợp lệ");
@@ -76,7 +99,21 @@ public class EditProfileActivity extends AppCompatActivity {
             tv_Ten.setText("");
             b = 0;
         }
-        if (a == 0 && b == 0) {
+        if (pass.length() <=6) {
+            tv_Pass.setText("Password phải chứa ít nhất 6 ký tự");
+            c = 1;
+        }else {
+            tv_Ten.setText("");
+            c = 0;
+        }
+        if (sdt.matches(String.valueOf(match.find() && match.group().equals(sdt)))) {
+            tv_Pass.setText("Số Điện Thoại Không Hợp Lệ");
+            d = 1;
+        }else {
+            tv_Pass.setText("");
+            d = 0;
+        }
+        if (a == 0 && b == 0 && c == 0 && d == 0) {
             return true;
         }
         return false;
